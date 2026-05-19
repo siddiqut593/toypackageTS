9c60f5852653c9b9f0c45938e4eb8d5169480229
siddiqut593 <siddiqut@lawrence.edu>
2026-05-19 14:16:55 +0000

    updated min_max_scale

--- R/min_max_scale.R
+++ R/min_max_scale.R
@@ -1,3 +1,13 @@
+#' Performs min-max scaling on a numeric vector
+#'
+#' @param x A numeric vector
+#'
+#' @returns A numeric vector
+#' @export
+#'
+#' @examples
+#' x <- 1:5
+#' min_max_scale(x)
 min_max_scale <- function(x)
 {
   (x - min(x, na.rm = TRUE)) / (max(x, na.rm = TRUE) - min(x, na.rm = TRUE))
