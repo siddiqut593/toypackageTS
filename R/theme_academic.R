bd94993038e2f7c39af92401de735e216774a3e8
siddiqut593 <siddiqut@lawrence.edu>
2026-05-19 14:24:56 +0000

    added roxygen for theme_academic

--- R/theme_academic.R
+++ R/theme_academic.R
@@ -1,3 +1,15 @@
+#' Applies theme_academic
+#'
+#' @param base_size Font size
+#'
+#' @returns A plot with this theme added
+#' @importFrom ggplot2 theme_classic theme element_blank element_text
+#' @export
+#'
+#' @examples
+#' library(ggplot2)
+#' p <- ggplot(mtcars) + geom_point(aes(x = mpg, y = hp)) + labs(title = "plot title")
+#' p + theme_academic()
 theme_academic <- function(base_size = 12) {
 
   ggplot2::theme_classic(base_size = base_size) +
